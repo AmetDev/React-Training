b53296c60b01ca90a7bd1a8f26d8e6f4142a752d
AmetJunior <ametsejdaliev@gmail.com>
2023-06-07 15:47:22 +0300

    last

--- win-coun01/src/App.jsx
+++ win-coun01/src/App.jsx
@@ -33,7 +33,7 @@ const Person = ()=> {
        <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
         <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
-        <div>{renderNames()}</div>
+        <div className="m-2">{renderNames()}</div>
     </form>  
    </div>
   )
@@ -42,9 +42,11 @@ const Person = ()=> {
 
   return (
     <div>
+      <h1>counter</h1>
       <div className="m-2">{counter}</div>
       <button className="m-2" onClick={handleIncrement}>+</button>
       <button className="m-2" onClick={handleDecrement}>-</button>
+      <h1>Name</h1>
       <Person/>
     </div>
   );

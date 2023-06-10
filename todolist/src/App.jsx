e7d2b84334eedf9941e74996f919946fc8f57188
AmetJunior <ametsejdaliev@gmail.com>
2023-06-10 13:56:59 +0300

    worker-handleSubmit

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
+
 function App() {
+  const [formData, setFormData] = useState({ name: "", submittedData: "" });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-  const [formData, setFromData] = useState('');
-  function clickMe(formD) {
-    console.log(formD)
-    return <div>{formD}</div>
-}
-const handle = (e) =>{
-  const {name, value} = e.target
-  console.log(name, value)
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData((prevData) => ({ ...prevData, submittedData: prevData.name, name: "" }));
+  };
+ 
 
-
-  const handleChange = (callback) => {
-    setFromData(formData)   
-    callback(formData.name)
-  }
-  
   return (
     <div className="App">
-      <input  value={formData.name} onChange={handle}  type="text" id="name" name="name"/>
-      <button type="button" onClick={()=>handleChange(clickMe)}>+</button>
-      <div>{clickMe(formData.name)}</div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <button type="submit">+</button>
+      </form>
+      <div>{formData.submittedData}</div>
     </div>
   );
 }

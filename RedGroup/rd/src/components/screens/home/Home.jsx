e721197e966d7a8a003e888f5c13d6d97632e1d5
AmetJunior <ametsejdaliev@gmail.com>
2023-07-04 15:05:10 +0300

    two-commit

--- RedGroup/rd/src/components/screens/home/Home.jsx
+++ RedGroup/rd/src/components/screens/home/Home.jsx
@@ -1,19 +1,21 @@
-//import car1 from './car1.jpg'
-export default function Home() {
-  return (
+//import car from './car1.jpg'
+import {cars} from './cars.data'
+import { CarItem } from './car-item/CarItem'
+
+function Home() {
+  // cars.map(car=> {
+  //   key = car.id
+  //   console.log(car.name)
+  // })
+  <div>
+    <h1>Cars catalog</h1>
     <div>
-      <h1 className="text-white text-4xl mt-4 mb-2">
-        Cars catalog
-      </h1>
-      <div>
-        <div className = "inline-block m-4 border-2 border-color: rgb(255 255 255);">
-          <img className="bg-cover w-100%" src="https://avatars.mds.yandex.net/get-autoru-vos/2087846/567fbfcff81d088c09eae25dd6dee7ba/456x342n" alt="bmw" />
-          <h2 className = "text-white pt-4">Car 1</h2>
-          <p  className= "text-white pt-4 pb-4">$ 1000000</p>
-          <button className="bg-white m-1 mb-4">Read more</button>
-        </div>
-      </div>
+      {cars.length ? (
+        cars.map(car => <CarItem key={car.id} car={car} />)
+      ): (
+        <p className='text-white'>There a no cars</p>
+      )}
     </div>
- 
-  )
+  </div>
 }
+export default Home;
---
+++ RedGroup/rd/src/components/screens/home/car-item/CarItem.jsx
@@ -0,0 +1,12 @@
+export function CarItem({car}) {
+    return ( 
+          <div>
+                  <div className='border inline-block m-5'>
+                  <img className='w-52 h-auto ' src={car.img} alt={car.name}></img>
+                  <div className='text-white'>{car.name}</div>
+                  <div className='text-white m-1'>{new Intl.NumberFormat("en", {style: "currency", currency: "USD"}).format(car.price)}</div>
+                  <button className='text-black bg-slate-400 rounded-md m-1 p-1'>read more</button>
+                 </div>
+            </div>  
+      )
+}

99e5244877ceb8c510fddbf7f4486931098e95a7
AmetDev <ametsejdaliev@gmail.com>
2023-08-21 22:35:26 +0300

    test

--- react-pizza/src/components/redux/store.js
+++ react-pizza/src/components/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filter from './slices/filterSlice'
 import cart from './slices/cartSlice'
+import pizzas from './slices/pizzasSlice'
 export const store = configureStore({
-	reducer: { filter, cart }
+	reducer: { filter, cart, pizzas }
 })
-

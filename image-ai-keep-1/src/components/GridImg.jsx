efab287ad7302af53517d6214f222d607b3e1bb5
AmetDev <ametsejdaliev@gmail.com>
2023-09-17 21:25:01 +0300

    one init useRef

--- image-ai-keep-1/src/components/GridImg.jsx
+++ image-ai-keep-1/src/components/GridImg.jsx
@@ -1,85 +1,84 @@
-import { useEffect, useState, useRef } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchImages, setCounter, setRef } from '../redux/slice/imageSlice'
-
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchImages, setCounter, setRef } from "../redux/slice/imageSlice";
 
 const GridImg = () => {
-	const imgFunc = (bool, element) => {
-		setSwitcher(bool)
-		setCurrentElement(element)
-	}
-	const dispatch = useDispatch()
-	const [switcher, setSwitcher] = useState(false)
-	const [currentElement, setCurrentElement] = useState({})
-	const { counter, images, error, isLoading, refClick } = useSelector(
-		state => state.images
-	)
-	useEffect(() => {
-		const func = async () => {
-			await dispatch(fetchImages({ counter }))
-		}
-		func()
-	}, [counter])
-	const nameRef = useRef('initialstate')
-	useEffect(()=>{
-		const handleCLick= (event) => {
-			if(!event.composedPath().includes(nameRef.current)) {
-				setSwitcher(false)
-				console.log(nameRef)
+  const imgFunc = (bool, element) => {
+    window.scrollTo(0, 0);
 
-				console.log("hello")
-			}
-		}
-		document.body.addEventListener("click", handleCLick)
-	})
-	const ImageItem = currentElement => {
-		return (
-			<div >
-				<div   className='bg-white flex '>
-					<img src={currentElement.url} alt='' />
-				<div>
-	<div className="m-5">{currentElement.title}</div>
-			</div>	
-		</div>
-			</div>
-		)
-	}
+    setSwitcher(bool);
+    setCurrentElement(element);
+  };
+  const dispatch = useDispatch();
+  const [switcher, setSwitcher] = useState(false);
+  const [currentElement, setCurrentElement] = useState({});
+  const { counter, images, error, isLoading, refClick } = useSelector(
+    (state) => state.images,
+  );
+  useEffect(() => {
+    const func = async () => {
+      await dispatch(fetchImages({ counter }));
+    };
+    func();
+  }, [counter]);
+  const nameRef = useRef("initialstate");
+  useEffect(() => {
+    const handleCLick = (event) => {
+      if (!event.composedPath().includes(nameRef.current)) {
+        event.preventDefault();
+        console.log("nameref.current", nameRef.current);
+        setSwitcher(false);
+      }
+    };
+    document.body.addEventListener("click", handleCLick);
+  });
+  const ImageItem = (currentElement) => {
+    return (
+      <div>
+        <div className="bg-white flex ">
+          <img src={currentElement.url} alt="" />
+          <div>
+            <div className="m-5">{currentElement.title}</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
-	return (
-		<>
-			{switcher && <ImageItem {...currentElement} />}
-			{isLoading ? (
-				<div>загрузка...{isLoading}</div>
-			) : error ? (
-				<div>{error}</div>
-			) : images ? (
-				<ul  className='grid grid-cols-3 gap-4 mt-5'>
-					{images.map(el => (
-						<li
-						   
-							onClick={() => imgFunc(true, el)}
-							className='w-[454px] h-[460px] m-2'
-							
-							key={el.id}
-							ref={nameRef}
-						>
-							<img className='rounded-[50px]' src={el.url} alt={el.title} />
-						</li>
-					))}
-				</ul>
-			) : (
-				''
-			)}
+  return (
+    <>
+      {switcher && <ImageItem {...currentElement} />}
+      {isLoading
+        ? <div>загрузка...{isLoading}</div>
+        : error
+        ? <div>{error}</div>
+        : images
+        ? (
+          <ul ref={nameRef} className="grid grid-cols-3 gap-4 mt-5">
+            {images.map((el) => (
+              <li
+                onClick={() => imgFunc(true, el)}
+                className="w-[454px] h-[460px] m-2"
+                key={el.id}
+              >
+                <img className="rounded-[50px]" src={el.url} alt={el.title} />
+              </li>
+            ))}
+          </ul>
+        )
+        : (
+          ""
+        )}
 
-			<button
-				onClick={() => dispatch(setCounter())}
-				className='bg-white w-16 h-8'
-				type='button'
-			>
-				<h1>MORE...</h1>
-			</button>
-		</>
-	)
-}
+      <button
+        onClick={() => dispatch(setCounter())}
+        className="bg-white w-16 h-8"
+        type="button"
+      >
+        <h1>MORE...</h1>
+      </button>
+    </>
+  );
+};
 
-export default GridImg
+export default GridImg;
